e82b6b79e6f860da8a98d6e2ecfe336fd1d1c4ef
Ansh Gupta <guptaansh912@gmail.com>
2024-06-26 22:30:46 +0530

    t

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,28 +2,59 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
-      <div className="footer wrapper">
-        <div className="container">
+    <footer className="bg-light text-center text-lg-start">
+        <div className="container p-4">
           <div className="row">
-            <div className="col-lg-4 col-md-6 mb-lg-0 mb-md-0 mb-5">
-              <h4>Hours</h4>
-              <p>Monday - Friday: 8am - 8pm</p>
-              <p>Saturday - Sunday: 8am - 6pm</p>
-            </div>
-            <div className="col-lg-4 col-md-6 mb-lg-0 mb-md-0 mb-5">
-              <h4>Contact Us</h4>
-              <p>3456+123 London, UK</p>
-              <p>+123 456 78910</p>
-            </div>
-            <div className="col-lg-4 col-md-12">
-              <h4>About</h4>
+            <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Foodera</h5>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum in sit amet leo. Mauris feugiat erat tellus.</p>
+            </div>
+            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Menu</h5>
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <a href="#!" className="text-dark">Home</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-dark">About</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-dark">Explore Food</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-dark">Reviews</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-dark">FAQ</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-dark">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Follow Us</h5>
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <a href="#!" className="text-dark">Facebook</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-dark">Twitter</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-dark">Instagram</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-dark">LinkedIn</a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-      </div>
-    </footer>
+        <div className="text-center p-3 bg-dark text-white">
+          &copy; 2024 Foodera. All Rights Reserved.
+        </div>
+      </footer>
   );
 }
 
