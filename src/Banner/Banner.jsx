406fbfb6a250a1143fc8a2df9564a6db72d2cb7f
gangwar22 <147224060+gangwar22@users.noreply.github.com>
2024-07-02 12:16:00 +0530

    Update Banner.jsx

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -8,13 +8,10 @@ function Banner() {
             <div className="row">
               <div className="col-lg-5 col-md-6">
                 <h1 style={{ color: 'white' }}>Good food choices are good investments.</h1>
-                <p style={{ color: 'black' }}>
+                <p style={{ color: 'white' }}>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum amet leo.
                 </p>
-                <div className="mt-4">
-                  <button className="main-btn">Order now <i className="fas fa-shopping-basket ps-3"></i></button>
-                  <button className="white-btn ms-lg-4 mt-lg-0 mt-4">Order now <i className="fas fa-angle-right ps-3"></i></button>
-                </div>
+               
               </div>
             </div>
           </div>
